97727ea1577e59ea7757ff5b56c812ca18855ed2
Rem <remerico@gmail.com>
2015-06-09 23:07:45 +0800

    Initial commit

---
+++ app/src/main/java/umuulanba/remerico/ph/umuulanba/MainActivity.java
@@ -0,0 +1,101 @@
+package umuulanba.remerico.ph.umuulanba;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONObject;
+
+/**
+ * Created by rem on 6/9/15.
+ */
+public class MainActivity extends Activity {
+
+    OkHttpClient client = new OkHttpClient();
+
+    View loadingView;
+    View contentView;
+
+    TextView messageText;
+    TextView regionText;
+    TextView forecastText;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        loadingView = findViewById(R.id.loading_view);
+        contentView = findViewById(R.id.content_view);
+
+        messageText = (TextView) findViewById(R.id.message_text);
+        regionText = (TextView) findViewById(R.id.region_text);
+        forecastText = (TextView) findViewById(R.id.forecast_text);
+
+        new WeatherLoader().execute();
+
+    }
+
+
+    private void setLoading(boolean loading) {
+        loadingView.setVisibility(loading ? View.VISIBLE : View.GONE);
+        contentView.setVisibility(loading ? View.GONE : View.VISIBLE);
+    }
+
+
+    private void setData(WeatherData wd) {
+        if (wd != null) {
+            messageText.setText(wd.isRainy() ? R.string.oo_tanga : R.string.hindi_gago);
+            forecastText.setText(wd.condition);
+            regionText.setText(wd.city + ", " + wd.region);
+        }
+        else {
+            messageText.setText("Error");
+        }
+
+    }
+
+
+    class WeatherLoader extends AsyncTask<Void, Void, JSONObject> {
+
+        @Override
+        protected void onPreExecute() {
+            setLoading(true);
+        }
+
+        @Override
+        protected JSONObject doInBackground(Void... voids) {
+
+            String address = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%20%271199477%27&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+
+            Request request = new Request.Builder()
+                    .url(address)
+                    .build();
+
+            try {
+                Response response = client.newCall(request).execute();
+                return new JSONObject(response.body().string());
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject result) {
+            setLoading(false);
+            setData(WeatherData.parseJson(result));
+        }
+
+    }
+
+}
---
+++ app/src/main/java/umuulanba/remerico/ph/umuulanba/WeatherData.java
@@ -0,0 +1,76 @@
+package umuulanba.remerico.ph.umuulanba;
+
+import org.json.JSONObject;
+
+/**
+ * Created by rem on 6/9/15.
+ */
+public class WeatherData {
+
+    public int code;
+    public String date;
+    public int temp;
+    public String condition;
+
+    public String city;
+    public String region;
+
+    public int temperatureCelsius() {
+        return ((temp - 32)*5)/9;
+    }
+
+    public boolean isRainy() {
+
+        return (code == 1
+                || code == 2
+                || code == 3
+                || code == 4
+                || code == 5
+                || code == 6
+                || code == 8
+                || code == 9
+                || code == 10
+                || code == 11
+                || code == 12
+                || code == 35
+                || code == 37
+                || code == 38
+                || code == 39
+                || code == 40
+                || code == 45
+                || code == 47);
+
+    }
+
+    public static WeatherData parseJson(JSONObject j) {
+
+        try {
+
+            WeatherData wd = new WeatherData();
+
+            JSONObject channel = j.getJSONObject("query").getJSONObject("results").getJSONObject("channel");
+
+            JSONObject location = channel.getJSONObject("location");
+
+            wd.city = location.optString("city");
+            wd.region = location.optString("region");
+
+            JSONObject condition = channel.getJSONObject("item").getJSONObject("condition");
+
+            wd.code = condition.optInt("code");
+            wd.date = condition.optString("date");
+            wd.temp = condition.optInt("temp");
+            wd.condition = condition.optString("text");
+
+            return wd;
+
+        }
+
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+}
